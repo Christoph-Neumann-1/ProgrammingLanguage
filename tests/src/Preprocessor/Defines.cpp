14109c18ebc466c06cebc0b81cd6bcf37dddb1d2
Christoph Neumann <christophneumann2005@gmail.com>
2021-10-19 18:50:21 +0200

    Shortened test
    Signed-off-by: Christoph Neumann <christophneumann2005@gmail.com>

--- tests/src/Preprocessor/Defines.cpp
+++ tests/src/Preprocessor/Defines.cpp
@@ -2,7 +2,6 @@
 #include <catch2/catch_test_macros.hpp>
 
 using namespace VWA;
-
 SCENARIO("Defining and later using a macro")
 {
     GIVEN("A file")
@@ -14,47 +13,42 @@ SCENARIO("Defining and later using a macro")
             f.append("##define macro expanded");
             THEN("Using ifdef should be true")
             {
-                auto tmp = f;
-                tmp.append("##ifdef macro");
-                tmp.append("true");
-                tmp.append("##endif");
-                REQUIRE(preprocess(tmp, logger).toString() == "true");
+                f.append("##ifdef macro");
+                f.append("true");
+                f.append("##endif");
+                REQUIRE(preprocess(f, logger).toString() == "true");
             }
             WHEN("The macro is expanded")
             {
-                auto tmp = f;
-                tmp.append("#macro");
                 THEN("The content of the macro should be pasted")
                 {
-                    REQUIRE(preprocess(tmp, logger).toString() == "expanded");
+                    f.append("#macro");
+                    REQUIRE(preprocess(f, logger).toString() == "expanded");
                 }
                 WHEN("There are other characters on the line")
                 {
-                    auto tmp2 = f;
-                    tmp2.append("before#macro after");
+                    f.append("before#macro after");
                     THEN("The everything preceding the macro should stay the same and everything after the space following the macro should stay the same")
                     {
-                        REQUIRE(preprocess(tmp2, logger).toString() == "beforeexpandedafter");
+                        REQUIRE(preprocess(f, logger).toString() == "beforeexpandedafter");
                     }
                 }
             }
             WHEN("Using nested macros")
             {
-                auto tmp = f;
-                tmp.append("##define macro2 #macro");
-                tmp.append("#macro2");
+                f.append("##define macro2 #macro");
+                f.append("#macro2");
                 THEN("The content of the macro should also be evaluated")
                 {
-                    REQUIRE(preprocess(tmp, logger).toString() == "expanded");
+                    REQUIRE(preprocess(f, logger).toString() == "expanded");
                 }
             }
             WHEN("Trying to expand an undefined macro")
             {
                 THEN("An exception should be thrown")
                 {
-                    auto tmp = f;
-                    tmp.append("#undefinedmacro");
-                    REQUIRE_THROWS_AS(preprocess(tmp, logger), PreprocessorException);
+                    f.append("#undefinedmacro");
+                    REQUIRE_THROWS_AS(preprocess(f, logger), PreprocessorException);
                 }
             }
         }
