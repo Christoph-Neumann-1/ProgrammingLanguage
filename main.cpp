ad9323d00202e83807ffe36d88d1c6596d34adbc
Christoph Neumann <christophneumann2005@gmail.com>
2021-10-25 10:34:33 +0200

    added more options to cli

--- main.cpp
+++ main.cpp
@@ -16,26 +16,51 @@ int main(int argc, char *argv[])
 
     std::string fileName;
 
-    app.add_option("-f,--file, file", fileName, "Input file")->required();
-
+    app.add_option("-f,--file, file", fileName, "Input file")->required()->check(CLI::ExistingFile);
+    std::string output="output.src";
+    auto outopt=app.add_option("-o,--output", output, "Output file");
+    bool toStdout=false;
+    app.add_flag("--stdout, !--nstdout",toStdout)->excludes(outopt);
+    //TODO: direct different loglevels to different outputs, use callbacks
+    std::string logFile;
+    app.add_option("-l,--log", logFile, "Log file");
     CLI11_PARSE(app, argc, argv);
 
-    VWA::ConsoleLogger logger;
+    std::unique_ptr<VWA::ILogger> logger;
+    if(logFile.empty())
+    {
+        logger=std::make_unique<VWA::ConsoleLogger>();
+    }
+    else
+    {
+        logger=std::make_unique<VWA::FileLogger>(logFile);
+    }
     std::ifstream file(fileName);
     if (!file.is_open())
     {
         std::cout << "File not found" << std::endl;
         return -1;
     }
+    VWA::File result;
     try
     {
-        std::cout << VWA::preprocess({.file = VWA::File{file, fileName}, .logger = logger}).toString() << std::flush;
+        result = VWA::preprocess({.file = VWA::File{file, fileName}, .logger = *logger});
     }
     catch (const VWA::PreprocessorException &e)
     {
         std::cout << "Preprocessor failed, see log for details" << std::endl;
         file.close();
         return -1;
+    }
+    if (toStdout)
+    {
+        std::cout << result.toString() << std::endl;
+    }
+    else
+    {
+        std::ofstream out(output);
+        out << result.toString();
+        out.close();
     }
     file.close();
     return 0;
